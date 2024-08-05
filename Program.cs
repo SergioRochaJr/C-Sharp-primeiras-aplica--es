ffde4c591f42ee35f2ba08d2445fc1b6ecb8b96d
Sérgio da Rocha <sergiorajr1994@gmail.com>
2024-08-05 14:40:46 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Screen Sound
+string mensagemDeBoasVindas = "Boas-vindas ao Screen Sound!";
+
+Console.WriteLine(mensagemDeBoasVindas);
+
+string curso = "Progamação em C#";
+
+Console.WriteLine(curso);
+
+string nome = "Ferdinand";
+string sobrenome = "Braum";
+string nomeCompleto = nome+" "+sobrenome;
+Console.WriteLine(nomeCompleto);
